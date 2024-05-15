08ea2664950eb132e4baba44c1778939ff6bd636
Aprina Pavlova <arinnymymail@mail.ru>
2024-05-15 22:09:48 +0300

    add streamlit

---
+++ steamlit/run.py
@@ -0,0 +1,128 @@
+from geopy.geocoders import Nominatim
+from etna.datasets import TSDataset
+from etna.models import CatBoostMultiSegmentModel
+from etna.transforms import LagTransform
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pandas as pd
+import pickle
+
+@st.cache_data
+def load_model_forcasting(model):
+    fit_model = model.load('../models/model_forcasting.sav')
+    return fit_model
+    
+@st.cache_data
+def load_model_kmeans():
+    filename = '../models/model_kmeans.sav'
+    model = pickle.load(open(filename, 'rb')) 
+    return model
+
+@st.cache_data
+def load_train_pd():
+    pd_train = pd.read_csv('../models/train.csv')
+    return pd_train
+
+@st.cache_data
+def load_test_pd():
+    pd_test = pd.read_csv('../models/test.csv')
+    return pd_test
+
+@st.cache_data
+def load_data_pd():
+    data = pd.read_csv('../models/data.csv')
+    return data
+
+def predict_clastering(lat, lon):
+    moscow_centre = (55.751003 * np.pi / 180.0, 37.617964 * np.pi / 180.0)  # перевод из градусов в радины
+    R = 6371.0 # км
+    x = (lat * np.pi / 180.0 - moscow_centre[0]) * R
+    y = (lon * np.pi / 180.0 - moscow_centre[1]) * R * np.cos(moscow_centre[0]) 
+    X = np.array([[x,y]])
+    model_kmeans = load_model_kmeans()  
+    number_claster = model_kmeans.predict([X[0]])[0]
+    return number_claster
+
+def predict_forcasting(number_claster : str):
+    data = load_data_pd()
+    ts = TSDataset(data, freq="MS")
+    train_ts, test_ts = ts.train_test_split(
+    train_start="2018-09-01",
+    train_end="2020-7-01",
+    test_start="2020-8-01",
+    test_end="2020-12-01",
+    )
+    model =  CatBoostMultiSegmentModel()
+    model = load_model_forcasting(model)
+    HORIZON = 5
+    lags = LagTransform(in_column="target", lags=[5])
+    transforms = [ lags]
+    train_ts.fit_transform(transforms)
+    future_ts = train_ts.make_future(future_steps=HORIZON, transforms=transforms)
+    forecast_ts = model.forecast(future_ts)
+    forecast_ts.inverse_transform(transforms)
+    return forecast_ts
+
+
+def predict_model():
+    if street != '':
+        st.write("Введите улицу")
+    elif house != '':
+        st.write("Введите дом")
+    
+    address = house + ', ' + street  
+    geolocator = Nominatim(user_agent="base")
+    location = geolocator.geocode(address)
+    print(location)
+    lat , lon = location.latitude, location.longitude
+    print(lat , lon)
+    # predict claster
+    number_claster = predict_clastering(lat, lon)
+    # find segment
+    k = number_claster
+    t = -1
+    r = number_room
+    if number_room == "Новостройка":
+        t = 11
+    elif number_room == "Вторичка":
+        t = 1
+
+    segment = f'k={k}_t={t}_r={r}'
+    # forcasting
+    forecast_ts = predict_forcasting(number_claster)
+    forecast_df = forecast_ts.to_pandas()
+    serias = forecast_df[segment]['target']
+
+    return serias
+  
+st.title("Предиктивная система для анализа цен на невдижемость", anchor=None, help=None)
+st.write("\n")
+st.header("Введите параметры жилья")
+
+st.subheader('Введите адрес жилья')
+street = st.text_input("Введите улицу")
+house = st.text_input("Введиет дом")
+
+st.subheader('Введите другие параметры')
+type_house = st.selectbox(
+    "Тип жилья",
+    ("Новостройка", "Вторичка"))
+number_room = st.selectbox(
+    "Количество комнат",
+    (1, 2, 3, 4))
+horizon = st.select_slider(
+    "Продолжительность предсказания",
+    options=[1, 2, 3, 4, 5, 6])
+
+
+if st.button('Predict!!!'):
+    serias = predict_model()
+    fig, ax = plt.subplots()
+    ax.plot(serias)
+    st.pyplot(fig)
+
+
+
+    
